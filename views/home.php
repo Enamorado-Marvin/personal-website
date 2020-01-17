73112fb4ca68ccfe2b922545d5b52fbe81f4fa4e
Marvin Enamorado <soycatracho@hotmail.com>
2020-01-17 16:42:26 -0600

    Edited homepage

--- views/home.php
+++ views/home.php
@@ -14,12 +14,12 @@
         <h1 class="orange">Family and Coding</h1>
         <hr>
         <p>
-          My two passions are my family and coding. When I am not with my kids and wife, I am in front of my computer creating things. With my family, I like to visit places, play with kids outside or sometimes we play Minecraft.</p>
+          My two passions are my family and coding. When I am not with my family, I am in front of my computer creating things. With my family, I like to visit places, play with the kids outside or sometimes we play Minecraft.</p>
         <p>
-          I have worked in very interesting projects. Most of them have to do with front-end and back-end. I built a website with a simple control panel where users can manage their catalog of products.
+          I have worked in very interesting projects. Most of them have to do with front-end and back-end web development. I built a website with a simple control panel where users can manage their catalog of products.
         </p>
         <p>
-          In this site, I have my personal information as a homepage, and the other part has my assignments completed for my CS 313 class.
+          In this site, I have my personal information as a homepage, and the other part has my completed assignments for my CS 313 class.
         </p>
       </div>
     </div>
