1a7becf0a31ea47317325a885c52f65d67bf6c4c
Marvin Enamorado <soycatracho@hotmail.com>
2020-02-20 06:22:41 -0600

    edited

--- Sacrament-meeting/model/dbConnect.php
+++ Sacrament-meeting/model/dbConnect.php
@@ -1,61 +1,40 @@
 <?php
-/**********************************************************
-* File: dbConnect.php
-* Author: Br. Burton
-* 
-* Description: Shows how to connect using either local
-* OR Heroku credentials, depending on whether the code
-* is executing at heroku.
-***********************************************************/
 
 function get_db() {
 	$db = NULL;
 
 	try {
-	// 	// default Heroku Postgres configuration URL
-	$dbUrl = getenv('DATABASE_URL');
+		// default Heroku Postgres configuration URL
+		$dbUrl = getenv('DATABASE_URL');
 
-	
-	// 	// Get the various parts of the DB Connection from the URL
-	$dbopts = parse_url($dbUrl);
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			// My localhost configuration
+			$dbUrl = "postgres://postgres:inlove1982@localhost:5432/agenda";
+		}
 
-	$dbHost = $dbopts["host"];
-	$dbPort = $dbopts["port"];
-	$dbUser = $dbopts["user"];
-	$dbPassword = $dbopts["pass"];
-	$dbName = ltrim($dbopts["path"],'/');
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
 
-	// Create the PDO connection
-	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
 
-	// 	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
-	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
 	catch (PDOException $ex) {
-	// 	// If this were in production, you would not want to echo
-	// 	// the details of the exception.
-	echo "Error connecting to DB. Details: $ex";
-	ie();
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
 	}
 
 	return $db;
 
 
-/*try
-	{
-	  $user = 'postgres';
-	  $password = 'inlove1982';
-	  $db = new PDO('pgsql:host=localhost;dbname=agenda', $user, $password);
-
-	  // this line makes PDO give us an exception when there are problems,
-	  // and can be very helpful in debugging! (But you would likely want
-	  // to disable it for production environments.)
-	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $ex)
-	{
-	  echo 'Error!: ' . $ex->getMessage();
-	  die();
-	}
-	return $db;*/
 }
